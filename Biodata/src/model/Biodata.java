4644f561d855b13dbe49fea0b409f1721ed8b76a
denanaa <161725650+denanaa@users.noreply.github.com>
2024-11-10 21:35:19 +0700

    Latihan 2 Pertemuan 7

---
+++ Biodata/src/model/Biodata.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Biodata.src.model;
+
+/**
+ *
+ * @author ASUS
+ */
+
+public class Biodata {
+    private String id;
+    private String nama;
+    private String alamat;
+
+    // Getter dan Setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
