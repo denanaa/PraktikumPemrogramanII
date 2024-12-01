ae4f7647054db1e29c07dbf5e50bd56ea8c7cedc
denanaa <161725650+denanaa@users.noreply.github.com>
2024-12-01 19:55:35 +0700

    Add files via upload
    Pertemuan 9

---
+++ MembershipORM/src/view/main/MainButtonActionListener.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MembershipORM.src.view.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MainButtonActionListener implements ActionListener {
+    
+    private MainFrame mainFrame;
+
+    public MainButtonActionListener(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == mainFrame.getButtonJenisMember()){
+            mainFrame.showJenisMemberFrame();
+        } else {
+            mainFrame.showMemberFrame();
+        }
+    }   
+}
